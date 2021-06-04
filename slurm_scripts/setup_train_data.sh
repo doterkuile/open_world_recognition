a15479e1a74424bb7c5591b4d2e1c4972c09317f
David ter Kuile <doterkuile@gmail.com>
2021-06-04 12:07:18 +0200

    Added logs and slurm working

--- slurm_scripts/setup_train_data.sh
+++ slurm_scripts/setup_train_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=train_data_setup   	# create a short name for your job
+#SBATCH --output=logs/%x-%j.out                 # output_file
 #SBATCH --partition=general				# select partition
 #SBATCH --qos=short						# select quality of service
 #SBATCH --nodes=1                		# node count
@@ -7,7 +8,7 @@
 #SBATCH --cpus-per-task=1        		# cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem=4096                		# total memory per node (4 GB per cpu-core is default)
 #SBATCH --gres=gpu:1             		# number of gpus per node
-#SBATCH --time=00:20:00          		# total run time limit (HH:MM:SS)
+#SBATCH --time=00:05:00          		# total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        		# send mail when job begins
 #SBATCH --mail-type=end          		# send mail when job ends
 #SBATCH --mail-type=fail         		# send mail if job fails
@@ -20,12 +21,12 @@ conda_env=l2acenv
 package=open_world_recognition
 data_path=/tudelft.net/staff_umbrella/openworldrecognition/
 
-mkdir -p /tmp/$USER && cp -a $data_path$package /tmp/$USER
+#mkdir -p /tmp/$USER && cp -a $data_path$package /tmp/$USER
 
-module use /opt/insy/modulefiles
-module purge
+#module use /opt/insy/modulefiles
+#module purge
 
-module load miniconda
-conda activate $conda_env
+#module load miniconda
+#conda activate $conda_env
 
-python ../$python_script $config_file
+python $python_script $config_file
