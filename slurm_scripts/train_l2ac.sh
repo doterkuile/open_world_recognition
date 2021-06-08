a99c3d2e84132b25878583add4887f91ab60de46
David ter Kuile <doterkuile@gmail.com>
2021-06-08 10:09:04 +0200

    extra slurm_scripts

--- slurm_scripts/train_l2ac.sh
+++ slurm_scripts/train_l2ac.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=train_l2ac  	# create a short name for your job
+#SBATCH --job-name=train_l2ac_30_epoch  	# create a short name for your job
 #SBATCH --output=logs/%x-%j.out                 # output_file
 #SBATCH --partition=general				# select partition
 #SBATCH --qos=short						# select quality of service
 #SBATCH --nodes=1                		# node count
 #SBATCH --ntasks=1               		# total number of tasks across all nodes
-#SBATCH --cpus-per-task=4        		# cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem=16gb                		# total memory per node (4 GB per cpu-core is default)
-#SBATCH --gres=gpu:2             		# number of gpus per node
-#SBATCH --time=00:30:00          		# total run time limit (HH:MM:SS)
+#SBATCH --cpus-per-task=6        		# cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem=20gb                		# total memory per node (4 GB per cpu-core is default)
+#SBATCH --gres=gpu:4             		# number of gpus per node
+#SBATCH --time=03:00:00          		# total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        		# send mail when job begins
 #SBATCH --mail-type=end          		# send mail when job ends
 #SBATCH --mail-type=fail         		# send mail if job fails
--- slurm_scripts/train_l2ac_long.sh
+++ slurm_scripts/train_l2ac_long.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=train_l2ac  	# create a short name for your job
+#SBATCH --job-name=train_l2ac_50_e  	# create a short name for your job
 #SBATCH --output=logs/%x-%j.out                 # output_file
 #SBATCH --partition=general				# select partition
-#SBATCH --qos=short						# select quality of service
+#SBATCH --qos=long						# select quality of service
 #SBATCH --nodes=1                		# node count
 #SBATCH --ntasks=1               		# total number of tasks across all nodes
-#SBATCH --cpus-per-task=4        		# cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem=16gb                		# total memory per node (4 GB per cpu-core is default)
-#SBATCH --gres=gpu:2             		# number of gpus per node
-#SBATCH --time=00:30:00          		# total run time limit (HH:MM:SS)
+#SBATCH --cpus-per-task=8        		# cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem=20gb                		# total memory per node (4 GB per cpu-core is default)
+#SBATCH --gres=gpu:8             		# number of gpus per node
+#SBATCH --time=09:00:00          		# total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        		# send mail when job begins
 #SBATCH --mail-type=end          		# send mail when job ends
 #SBATCH --mail-type=fail         		# send mail if job fails
@@ -21,7 +21,7 @@ conda_env=l2acenv
 
 
 module use /opt/insy/modulefiles
-conda deactivate
+
 module purge
 
 
