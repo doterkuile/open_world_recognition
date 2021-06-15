bd79e575d585dd797adccd551050c3ed567abc1a
David ter Kuile <doterkuile@gmail.com>
2021-06-15 15:04:14 +0200

    edited slurm scripts

--- slurm_scripts/train_l2ac.sh
+++ slurm_scripts/train_l2ac.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=train_l2ac_top_n  	# create a short name for your job
+#SBATCH --job-name=train_l2ac_balanced_set  	# create a short name for your job
 #SBATCH --output=logs/%x-%j.out                 # output_file
 #SBATCH --partition=general				# select partition
 #SBATCH --qos=short						# select quality of service
 #SBATCH --nodes=1                		# node count
 #SBATCH --ntasks=1               		# total number of tasks across all nodes
 #SBATCH --cpus-per-task=6        		# cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem=10gb                		# total memory per node (4 GB per cpu-core is default)
+#SBATCH --mem=14gb                		# total memory per node (4 GB per cpu-core is default)
 #SBATCH --gres=gpu:2             		# number of gpus per node
 #SBATCH --time=04:00:00          		# total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        		# send mail when job begins
@@ -33,21 +33,36 @@ conda_env=l2acenv
 # Loop variables
 file=config/$config_file
 var_1=name	
-array_1=(0006 0007)
+array_1=(0029, 0030, 0031, 0032)
 var_2=top_n
-array_2=(1 2)
+array_2=(9, 9, 9, 9)
+var_3=top_k
+array_3=(5, 10, 20, 50)
 len=${#array_1[@]}
+
+
+
+var_4=epochs
+value_4=5
 
 conda activate $conda_env
 
 
 for ((i=0;i<$len; i++))
+
 do
+
+        echo "$var_4 = ${value_4}"
+        sed -i "s/$var_4:.*/$var_4: ${value_4}/" $file
+	
+
 	echo "$var_1 = ${array_1[$i]}"
 	echo "$var_2 = ${array_2[$i]}"
+        echo "$var_3 = ${array_3[$i]}"
 
 	sed -i "s/$var_1:.*/$var_1: '${array_1[$i]}'/"  $file
 	sed -i "s/$var_2:.*/$var_2: ${array_2[$i]}/" $file
+	sed -i "s/$var_3:.*/$var_3: ${array_3[$i]}/" $file
 
 
 	python $python_script $config_file
