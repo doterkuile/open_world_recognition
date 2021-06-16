f0f067479694d5bdb21fbfa35493d4439a74e4d2
David ter Kuile <doterkuile@gmail.com>
2021-06-16 10:11:06 +0200

    Amazon dataset run with 200 epochs exp 0037

--- slurm_scripts/train_l2ac.sh
+++ slurm_scripts/train_l2ac.sh
@@ -2,13 +2,13 @@
 #SBATCH --job-name=train_l2ac_balanced_set  	# create a short name for your job
 #SBATCH --output=logs/%x-%j.out                 # output_file
 #SBATCH --partition=general				# select partition
-#SBATCH --qos=short						# select quality of service
+#SBATCH --qos=long						# select quality of service
 #SBATCH --nodes=1                		# node count
 #SBATCH --ntasks=1               		# total number of tasks across all nodes
 #SBATCH --cpus-per-task=6        		# cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem=14gb                		# total memory per node (4 GB per cpu-core is default)
+#SBATCH --mem=9gb                		# total memory per node (4 GB per cpu-core is default)
 #SBATCH --gres=gpu:2             		# number of gpus per node
-#SBATCH --time=04:00:00          		# total run time limit (HH:MM:SS)
+#SBATCH --time=07:00:00          		# total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        		# send mail when job begins
 #SBATCH --mail-type=end          		# send mail when job ends
 #SBATCH --mail-type=fail         		# send mail if job fails
@@ -26,18 +26,18 @@ module load miniconda
 
 # configuration variables
 python_script=train_l2ac.py
-config_file=L2AC_train.yaml
+config_file=L2AC_amazon_train.yaml
 conda_env=l2acenv
 
 
 # Loop variables
 file=config/$config_file
 var_1=name	
-array_1=(0035 0036)
+array_1=(0037)
 var_2=top_n
-array_2=(9 9)
+array_2=(9)
 var_3=model_class
-array_3=(L2AC_no_lstm L2AC_extended_similarity)
+array_3=(L2AC)
 #array_3=(L2AC L2AC_cosine L2AC_no_lstm L2AC_extended_similarity)
 len=${#array_1[@]}
 
