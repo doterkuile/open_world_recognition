d6517924462072e6e94282ab89ad922e074d712c
david <doterkuile@gmail.com>
2021-07-06 09:58:33 +0200

    Added tinyimagenet for setup_train_data

--- setup_train_data.py
+++ setup_train_data.py
@@ -53,10 +53,10 @@ def main():
 
     tst_data_rep, tst_data_cls_rep, tst_labels_rep = meta_utils.extract_features(test_data, model, classes, feature_memory_path, load_memory)
 
-
-    print(f'Rank validation samples with {test_classes} classes, {train_samples_per_cls} samples per class')
+    tst_samples_per_cls = int(len(test_data)/tst_data_cls_rep.shape[0])
+    print(f'Rank validation samples with {test_classes} classes, {tst_samples_per_cls} samples per class')
     valid_X0, valid_X1, valid_Y = meta_utils.rank_samples_from_memory(classes[-test_classes:], tst_data_rep, tst_data_cls_rep,
-                                                                      tst_labels_rep, classes, 100, 1,
+                                                                      tst_labels_rep, classes, tst_samples_per_cls, 1,
                                                                       randomize_samples)
 
 
--- slurm_scripts/setup_train_data.sh
+++ slurm_scripts/setup_train_data.sh
@@ -6,9 +6,9 @@
 #SBATCH --nodes=1                		# node count
 #SBATCH --ntasks=1               		# total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        		# cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem=4096                		# total memory per node (4 GB per cpu-core is default)
+#SBATCH --mem=10gb                		# total memory per node (4 GB per cpu-core is default)
 #SBATCH --gres=gpu:1             		# number of gpus per node
-#SBATCH --time=00:15:00          		# total run time limit (HH:MM:SS)
+#SBATCH --time=03:00:00          		# total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        		# send mail when job begins
 #SBATCH --mail-type=end          		# send mail when job ends
 #SBATCH --mail-type=fail         		# send mail if job fails
@@ -17,7 +17,7 @@
 
 # configuration variables
 python_script=setup_train_data.py
-config_file=CIFAR100_train.yaml
+config_file=TinyImageNet_train.yaml
 conda_env=l2acenv
 
 
